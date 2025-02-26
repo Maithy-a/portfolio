f172567d25d83a60502fdca9a183268339e156db
Maithya <maithyabon2@gmail.com>
2025-02-27 00:30:34 +0300

    refactor: clean up styles and improve HTML structure

--- js/main.js
+++ js/main.js
@@ -1,15 +1,9 @@
-/*=================
- * Maithyaverse 1.0.0 - Main JS
- *
- * ------------------------------------------------------------------- */
+/* Maithyaverse 1.0.0 - Main JS */
 
 (function (html) {
-
     "use strict";
 
     html.className = html.className.replace(/\bno-js\b/g, '') + ' js ';
-
-
 
     /* Animations
      * -------------------------------------------------- */
@@ -65,12 +59,9 @@
             opacity: [0, 1]
         }, '-=800');
 
-
-
     /* Preloader
      * -------------------------------------------------- */
     const ssPreloader = function () {
-
         const preloader = document.querySelector('#preloader');
         if (!preloader) return;
 
@@ -89,14 +80,11 @@
         window.addEventListener('beforeunload', function () {
             window.scrollTo(0, 0);
         });
-
-    }; // end ssPreloader
-
+    };
 
     /* Mobile Menu
      * ---------------------------------------------------- */
     const ssMobileMenu = function () {
-
         const toggleButton = document.querySelector('.mobile-menu-toggle');
         const mainNavWrap = document.querySelector('.main-nav-wrap');
         const siteBody = document.querySelector("body");
@@ -111,7 +99,6 @@
 
         mainNavWrap.querySelectorAll('.main-nav a').forEach(function (link) {
             link.addEventListener("click", function (event) {
-
                 // at 800px and below
                 if (window.matchMedia('(max-width: 800px)').matches) {
                     toggleButton.classList.toggle('is-clicked');
@@ -121,44 +108,29 @@
         });
 
         window.addEventListener('resize', function () {
-
             // above 800px
             if (window.matchMedia('(min-width: 801px)').matches) {
                 if (siteBody.classList.contains('menu-is-open')) siteBody.classList.remove('menu-is-open');
                 if (toggleButton.classList.contains("is-clicked")) toggleButton.classList.remove("is-clicked");
             }
         });
-
-    }; // end ssMobileMenu
-
+    };
 
     /* Highlight active menu link on pagescroll
      * ------------------------------------------------------ */
     const ssScrollSpy = function () {
-
         const sections = document.querySelectorAll(".target-section");
 
-        // Add an event listener listening for scroll
         window.addEventListener("scroll", navHighlight);
 
         function navHighlight() {
-
-            // Get current scroll position
             let scrollY = window.pageYOffset;
 
-            // Loop through sections to get height(including padding and border), 
-            // top and ID values for each
             sections.forEach(function (current) {
                 const sectionHeight = current.offsetHeight;
                 const sectionTop = current.offsetTop - 50;
                 const sectionId = current.getAttribute("id");
 
-                /* If our current scroll position enters the space where current section 
-                 * on screen is, add .current class to parent element(li) of the thecorresponding 
-                 * navigation link, else remove it. To know which link is active, we use 
-                 * sectionId variable we are getting while looping through sections as 
-                 * an selector
-                 */
                 if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
                     document.querySelector(".main-nav a[href*=" + sectionId + "]").parentNode.classList.add("current");
                 } else {
@@ -166,24 +138,19 @@
                 }
             });
         }
-
-    }; // end ssScrollSpy
-
+    };
 
     /* Animate elements if in viewport
      * ------------------------------------------------------ */
     const ssViewAnimate = function () {
-
         const blocks = document.querySelectorAll("[data-animate-block]");
 
         window.addEventListener("scroll", viewportAnimation);
 
         function viewportAnimation() {
-
             let scrollY = window.pageYOffset;
 
             blocks.forEach(function (current) {
-
                 const viewportHeight = window.innerHeight;
                 const triggerTop = (current.offsetTop + (viewportHeight * .2)) - viewportHeight;
                 const blockHeight = current.offsetHeight;
@@ -206,47 +173,11 @@
                 }
             });
         }
-
-    }; // end ssViewAnimate
-
-
-    /* Swiper
-     * ------------------------------------------------------ */
-    const ssSwiper = function () {
-
-        const mySwiper = new Swiper('.swiper-container', {
-
-            slidesPerView: 1,
-            pagination: {
-                el: '.swiper-pagination',
-                clickable: true,
-            },
-            breakpoints: {
-                // when window width is > 400px
-                401: {
-                    slidesPerView: 1,
-                    spaceBetween: 20
-                },
-                // when window width is > 800px
-                801: {
-                    slidesPerView: 2,
-                    spaceBetween: 32
-                },
-                // when window width is > 1200px
-                1201: {
-                    slidesPerView: 2,
-                    spaceBetween: 80
-                }
-            }
-        });
-
-    }; // end ssSwiper
-
+    };
 
     /* Lightbox
      * ------------------------------------------------------ */
     const ssLightbox = function () {
-
         const folioLinks = document.querySelectorAll('.folio-list__item-link');
         const modals = [];
 
@@ -256,7 +187,6 @@
                 document.querySelector(modalbox),
                 {
                     onShow: function (instance) {
-                        //detect Escape key press
                         document.addEventListener("keydown", function (event) {
                             event = event || window.event;
                             if (event.keyCode === 27) {
@@ -275,53 +205,12 @@
                 modals[index].show();
             });
         });
+    };
 
-    };  // end ssLightbox
-
-
-    /* Alert boxes
-     * ------------------------------------------------------ */
-    const ssAlertBoxes = function () {
-
-        const boxes = document.querySelectorAll('.alert-box');
-
-        boxes.forEach(function (box) {
-
-            box.addEventListener('click', function (event) {
-                if (event.target.matches(".alert-box__close")) {
-                    event.stopPropagation();
-                    event.target.parentElement.classList.add("hideit");
-
-                    setTimeout(function () {
-                        box.style.display = "none";
-                    }, 500)
-                }
-            });
-
-        })
-
-    }; // end ssAlertBoxes
-
-
-    /* Smoothscroll
+    /* Smooth scroll
      * ------------------------------------------------------ */
     const ssMoveTo = function () {
-
         const easeFunctions = {
-            easeInQuad: function (t, b, c, d) {
-                t /= d;
-                return c * t * t + b;
-            },
-            easeOutQuad: function (t, b, c, d) {
-                t /= d;
-                return -c * t * (t - 2) + b;
-            },
-            easeInOutQuad: function (t, b, c, d) {
-                t /= d / 2;
-                if (t < 1) return c / 2 * t * t + b;
-                t--;
-                return -c / 2 * (t * (t - 2) - 1) + b;
-            },
             easeInOutCubic: function (t, b, c, d) {
                 t /= d / 2;
                 if (t < 1) return c / 2 * t * t * t + b;
@@ -342,23 +231,17 @@
         triggers.forEach(function (trigger) {
             moveTo.registerTrigger(trigger);
         });
-
-    }; // end ssMoveTo
-
+    };
 
     /* Initialize
      * ------------------------------------------------------ */
     (function ssInit() {
-
         ssPreloader();
         ssMobileMenu();
         ssScrollSpy();
         ssViewAnimate();
-        ssSwiper();
         ssLightbox();
-        ssAlertBoxes();
         ssMoveTo();
-
     })();
 
 })(document.documentElement);
